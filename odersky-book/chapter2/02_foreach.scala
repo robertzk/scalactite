8f4a7d12fc50bce9ca8ae0f9326641fc1f2b7516
RobertZK <technoguyrob@gmail.com>
2015-03-13 22:04:41 -0500

    new instantiation

--- odersky-book/chapter2/02_foreach.scala
+++ odersky-book/chapter2/02_foreach.scala
@@ -1,1 +1,6 @@
 List(1,2,3).foreach(x => println(x + 1))
+// Can add type annotation
+List(1,2,3).foreach((x: Int) => println(x + 1))
+
+// Can even use a for expression
+for(x <- List(1,2,3)) println(x + 1)
---
+++ odersky-book/chapter3/01_instantiation.scala
@@ -0,0 +1,2 @@
+val big = new java.math.BigInteger("12345")
+big.multiply(new java.math.BigInteger("54321"))
